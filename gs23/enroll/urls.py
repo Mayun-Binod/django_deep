93282a13f95324a51b01174fde2bff582d960359
Mayun-binod <shresthabinod334@gmail.com>
2024-10-31 10:02:08 +0545

    added

--- gs23/enroll/urls.py
+++ gs23/enroll/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('stu/',views.studentinfo),
+    path('registration/',views.showformdata),
 ]
--- gs23/enroll/views.py
+++ gs23/enroll/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from enroll.models import Student
+from enroll.forms import StudentRegistration
 # Create your views here.
 
 def studentinfo(request):
     stud=Student.objects.all()
     return render(request,'enroll/studetails.html',{'stu':stud})
+
+def showformdata(request):
+    fm=StudentRegistration()
+    return render(request,'enroll/userregistration.html',{'form':fm})
