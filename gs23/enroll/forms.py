09cf56d5b34c345ee39f069799555db66fc17bde
Mayun-binod <shresthabinod334@gmail.com>
2024-10-31 18:43:22 +0545

    added

--- gs23/enroll/forms.py
+++ gs23/enroll/forms.py
@@ -2,3 +2,13 @@ from django import forms
 class StudentRegistration(forms.Form):
     name=forms.CharField()
     email=forms.EmailField()
+    first_name=forms.CharField()
+
+
+
+
+
+
+
+
+    
--- gs23/enroll/views.py
+++ gs23/enroll/views.py
@@ -8,5 +8,6 @@ def studentinfo(request):
     return render(request,'enroll/studetails.html',{'stu':stud})
 
 def showformdata(request):
-    fm=StudentRegistration()
+    fm=StudentRegistration(auto_id=True,label_suffix='B ',initial={'name':'Binod','email':'shresthabinod334@gmail.com'})
+    fm.order_fields(field_order=['first_name','name','email'])
     return render(request,'enroll/userregistration.html',{'form':fm})
