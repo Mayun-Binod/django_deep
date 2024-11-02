354c682d31752369096d06a3df4f08f795b90d81
Mayun-binod <shresthabinod334@gmail.com>
2024-11-02 07:55:21 +0545

    added

--- gs23/enroll/forms.py
+++ gs23/enroll/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 class StudentRegistration(forms.Form):
-    name=forms.CharField()
+    name=forms.CharField(help_text="help me")
     email=forms.EmailField()
     first_name=forms.CharField()
 
